575cf34c8a4eb37d89e4b7c80400918e4fb4848d
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2024-07-17 06:58:32 +0300

    modify index page

--- app/index.jsx
+++ app/index.jsx
@@ -1,24 +1,24 @@
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 import WelcomeScreen from "../components/WelcomeScreen";
 import OnBoardingScreenThree from "../components/OnBoardingSCreenThree";
 import OnBoardingScreenTwo from "../components/OnBoardingScreenTwo";
 import OnBoardingScreenOne from "../components/OnBoardingScreenOne";
 import RidelinkButton from "../components/RidelinkButton";
-import { router } from "expo-router";
+import { Link, router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-
-const primaryColor = "rgb(31,144,130)";
+import { primaryColor } from "../constants";
 
 export default function Page() {
   const [display, setDisplay] = useState(0);
-  useState;
+
   return (
     <View style={styles.container}>
       {display == 0 ? <WelcomeScreen /> : null}
       {display == 1 ? <OnBoardingScreenOne /> : null}
       {display == 2 ? <OnBoardingScreenTwo /> : null}
       {display == 3 ? <OnBoardingScreenThree /> : null}
+      <Link href="/(drawer)">Dashboard</Link>
       <RidelinkButton
         title="Next"
         onPress={() => {
@@ -31,36 +31,19 @@ export default function Page() {
         style={{ marginBottom: 50 }}
       />
       <View style={styles.indicatorContainer}>
-        <View
-          style={[
-            styles.indicator,
-            display === 0 ? { backgroundColor: primaryColor } : null,
-          ]}
-          onClick={() => setDisplay(0)}
-        />
-        <View
-          style={[
-            styles.indicator,
-            display === 1 ? { backgroundColor: primaryColor } : null,
-          ]}
-          onClick={() => setDisplay(1)}
-        />
-        <View
-          style={[
-            styles.indicator,
-            display === 2 ? { backgroundColor: primaryColor } : null,
-          ]}
-          onClick={() => setDisplay(2)}
-        />
-        <View
-          style={[
-            styles.indicator,
-            display === 3 ? { backgroundColor: primaryColor } : null,
-          ]}
-          onClick={() => setDisplay(3)}
-        />
+        {[0, 1, 2, 3].map((index) => (
+          <Pressable
+            key={index}
+            style={[
+              styles.indicator,
+              display === index ? { backgroundColor: primaryColor } : null,
+            ]}
+            onPress={() => setDisplay(index)}
+          />
+        ))}
       </View>
-      <StatusBar style="dark" />
+
+      <StatusBar style="dark" translucent />
     </View>
   );
 }
