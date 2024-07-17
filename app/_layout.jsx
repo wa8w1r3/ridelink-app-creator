ee97d6b1b6f4736e46a384eb49419080b7c44422
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2024-07-17 07:00:18 +0300

    Modify root layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,4 +1,4 @@
-import { Slot, Stack } from "expo-router";
+import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function Layout() {
